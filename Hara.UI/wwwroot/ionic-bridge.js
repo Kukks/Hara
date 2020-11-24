2513544d01cfca9c7a8a1fdad1d860e148a56f7b
Kukks <evilkukka@gmail.com>
2020-11-24 14:07:18 +0100

    poc for ionic laoder

--- Hara.UI/wwwroot/ionic-bridge.js
+++ Hara.UI/wwwroot/ionic-bridge.js
@@ -1,5 +1,22 @@
-window.IonicBridge = {
-    present: function(element){
-        element.present();
+function executeFunctionByName(ns, name)
+{
+    if(typeof ns === "string"){
+        ns = document.querySelector(ns);
     }
+    var args = Array.prototype.slice.call(arguments).splice(2);
+    if(!ns || !ns[name]){
+        console.error(`"Blazor attempted to call ${ns}.${name}(${args.join(', ')})`)
+        return;
+    }
+    return ns[name].apply(ns, args);
+}
+
+function registerBlazorCustomHandler(component, eventName, callbackClass, callBackMethodName) {
+    component.addEventListener(eventName, (e) => {
+        callbackClass.invokeMethodAsync(callBackMethodName, e.detail);
+    });
+}
+
+function setWebComponentProperty(webComp, propertyName, value) {
+    webComp[propertyName] = value;
 }
