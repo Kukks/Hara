077e36ef9e15cc2623eab0732a340a250d983430
Kukks <evilkukka@gmail.com>
2020-12-06 11:17:31 +0100

    WIP: Introduce Electron.NET for windows and linux
    Support BlazorMobileBindings for Linux is completely missing and Windows support is very limited. This attempts to introduce using the SSB build in conjunction with Electron

--- Hara.Server/Startup.cs
+++ Hara.Server/Startup.cs
@@ -26,7 +26,7 @@ namespace Hara.Server
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public virtual void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
@@ -47,7 +47,7 @@ namespace Hara.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
